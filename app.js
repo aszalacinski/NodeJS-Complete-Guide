3278b49ae306dea9317282c43df6322dbd89ec83
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2020-03-28 15:28:37 -0400

    Basic setup

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const feedRoutes = require('./routes/feed');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use(bodyParser.json());
 
-server.listen(3000);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', [
+        'GET',
+        'HEAD',
+        'OPTIONS',
+        'POST',
+        'PUT',
+        'DELETE',
+        'PATCH'
+    ]);
+    res.setHeader('Access-Control-Allow-Headers', [
+        'Authorization',
+        'Access-Control-Allow-Headers',
+        'Origin',
+        'Accept',
+        'X-Requested-With',
+        'Content-Type',
+        'Access-Control-Request-Method',
+        'Access-Control-Request-Headers'
+    ]);
+    next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.listen(8080);
