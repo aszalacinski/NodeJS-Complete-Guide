2a041d499a19970d0901c895f9ca7ab27b04827c
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2020-04-12 14:47:32 -0400

    init

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
+const graphqlHttp = require('express-graphql');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -53,11 +56,51 @@ app.use((req, res, next) => {
         'Authorization',
         'Content-Type'
     ]);
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+    if (!req.isAuth) {
+        throw new Error('Not authenticated.');
+    }
+    if (!req.file) {
+        return res.status(200).json({ message: 'No file provided!' });
+    }
+    if (req.body.oldPath) {
+        clearImage(req.body.oldPath);
+    }
+    return res
+        .status(201)
+        .json({ message: 'File stored.', filePath: req.file.path });
+});
+
+app.use(
+    '/graphql',
+    graphqlHttp({
+        schema: graphqlSchema,
+        rootValue: graphqlResolver,
+        graphiql: true,
+        customFormatErrorFn(err) {
+            if (!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || 'An error occurred.';
+            const code = err.originalError.code || 500;
+
+            return {
+                message: message,
+                status: code,
+                data: data
+            };
+        }
+    })
+);
 
 app.use((error, req, res, next) => {
     console.log(error);
@@ -73,10 +116,11 @@ mongoose.set('useFindAndModify', false);
 mongoose
     .connect('mongodb://localhost/messages?retryWrites=true')
     .then(result => {
-        const server = app.listen(8080);
-        const io = require('./socket').init(server);
-        io.on('connection', socket => {
-            console.log('Client connected');
-        });
+        app.listen(8080);
     })
     .catch(err => console.log(err));
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
+};
